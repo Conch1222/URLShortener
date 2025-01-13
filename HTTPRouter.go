9428a2aace0878bf0b862c4a17f3103cebce47db
Tim He <tim.he@sunbirddcim.com>
2025-01-13 18:00:08 +0800

    init files

---
+++ HTTPRouter.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+func setRouter() {
+	r := gin.Default()
+
+	r.POST("/api/shorten", shorten)
+
+	err := r.Run("8080") //set port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
